138c3ac9c0f862de67982f005724d9885af679d5
sergisantos658 <sergisantos658@gmail.com>
2022-02-08 16:18:42 +0100

    push game

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform targetObject;
+    public Vector3 offsset;
+
+    public float smoothFactor = 0.5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPosition = targetObject.transform.position + offsset;
+        transform.position = Vector3.Slerp(transform.position, newPosition, smoothFactor);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 7;
+    public Rigidbody rb;
+    float horizontalImput;
+    void FixedUpdate()
+    {
+        Vector3 ForwardMove = -transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = -transform.right * horizontalImput * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + ForwardMove + horizontalMove);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalImput = Input.GetAxis("Horizontal");
+    }
+}
